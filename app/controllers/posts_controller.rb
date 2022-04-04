0c228c658014d8cf6a9363690c690d96810e96b2
izureco <freestyleaerial@gmail.com>
2022-04-04 14:39:13 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,16 @@ class PostsController < ApplicationController
   # end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content])
+      # フォームから入力された値はparamsに入っているので、変数postに格納しておく。
+
+    # ▼は"レスポンスはHTML"だと指定している。今回は非同期通信なので、コメントアウトする
+    # redirect_to action: :index
+
+    # 非同期通信のレスポンスはJSON形式なので, renderメソッドを使ってJSONに変更する。
+    render json:{ post: post }
+      # render json:{ キー名: 変数名 }
+      # jsonオプションを用いると、{}内のキーとバリューをセットにして、json形式でレスポンスしてくれる。
+
   end
 end
